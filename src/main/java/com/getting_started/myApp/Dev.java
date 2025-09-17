ca698ad7385f446fe877ab9f726f046f5491a09b
aaryyya <aryampatil2005@gmail.com>
2025-09-17 19:59:04 +0530

    @Component, beans and objects

---
+++ src/main/java/com/getting_started/myApp/Dev.java
@@ -0,0 +1,10 @@
+package com.getting_started.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on this Awesome Project");
+    }
+}
--- src/main/java/com/getting_started/myApp/MyAppApplication.java
+++ src/main/java/com/getting_started/myApp/MyAppApplication.java
@@ -2,12 +2,15 @@ package com.getting_started.myApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyAppApplication.class, args);
-	}
+        ApplicationContext context= SpringApplication.run(MyAppApplication.class, args);
+        Dev obj = context.getBean(Dev.class);
+        obj.build();
+    }
 
 }
